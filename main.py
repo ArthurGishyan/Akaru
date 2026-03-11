c6805511eccbbaa756729be5de542cf4fa3bf7c1
Arthur <arthurgishyan2006@gmail.com>
2026-03-11 19:47:08 +0400

    Update initial figlet display text to 'Baylus'.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 from termcolor import cprint 
 from pyfiglet import figlet_format
 
-cprint(figlet_format('Sovoreq axper', font='starwars'),
+cprint(figlet_format('Baylus', font='starwars'),
        'yellow', 'on_red', attrs=['bold'])
